6224011dab3b5a367576f7172631460af9d30524
Spyxxer <demilade.omosehin@gmail.com>
2024-04-08 03:19:37 +0100

    changes made

--- mxrpearn.py
+++ mxrpearn.py
@@ -2,14 +2,15 @@ import requests
 import time
 import json, sys
 import threading
+import multiprocessing
 
-def countdown(seconds):
+def countdown(seconds, user):
     while seconds > 0:
         mins, secs = divmod(seconds, 60)
         timer = '{:02d}:{:02d}'.format(mins, secs)
         print(timer, end='\r')  # Print timer on the same line
         time.sleep(1); seconds -= 1
-    print('Countdown complete!')
+    print(f'Countdown complete! on {user}')
 
 def collect_details():
 	email = input("Insert your email or username: ")
@@ -54,18 +55,17 @@ def activate_token(session, user):
 		url = "https://faucetearner.org/api.php?act=get_faucet"
 		headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:123.0) Gecko/20100101 Firefox/123.0",
 		"X-Requested-With":"XMLHttpRequest", "Content-Type":"application/json"}
-
 		response = session.post(url, headers=headers)
 		if response.status_code == 200:
 			text = json.loads(response.text)
-			collect_token(session); count += 1
-			print(text["message"]); print(f"\nClaims on user: {user} = {count}")
-			countdown(60)
+			collect_token(session, user); count += 1
+			print(text["message"]); print(f"\nClaims on user: {user} = [{count}]")
+			countdown(60, user)
 		else:
-			print("An error occured here..")
-			print(response.text)
+			print(f"An error occured here on activating token..{user}"); 
+			print(response.status_code)
 
-def collect_token(session):
+def collect_token(session, user):
 	url = "https://faucetearner.org/api.php?act=faucet"
 	headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:123.0) Gecko/20100101 Firefox/123.0",
 	"X-Requested-With":"XMLHttpRequest", "Content-Type":"application/json"}
@@ -74,8 +74,8 @@ def collect_token(session):
 		text = json.loads(response.text)
 		print(); print(text["message"])
 	else:
-		print("An error occured here..")
-		print(response.text)
+		print(f"An error occured here in collecting token..{user}")
+		print(response.status_code)
 		
 
 with open('user_data.txt', 'r') as file:
@@ -84,8 +84,8 @@ with open('user_data.txt', 'r') as file:
 		line = line.strip()
 		user, mail, pwd = line.split(" ")
 		try:
-			thread = threading.Thread(target=login, args=(user, pwd, i)); thread.start(); 
-			i += 1; time.sleep(5)
+			thread = threading.Thread(target=login, args=(user, pwd, i)); thread.start();
+			i += 1; time.sleep(30)
 		except Exception as e:
 			print(e); break
 
